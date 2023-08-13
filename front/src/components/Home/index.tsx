4723986e2f2c7d516cef15a49a553b11b51f5b56
Sky_Taill <vorobeichik2015@yandex.ru>
2023-08-14 01:41:20 +0300

    add home page and first login page

--- front/src/components/Home/index.tsx
+++ front/src/components/Home/index.tsx
@@ -15,14 +15,28 @@ const Home: React.FC = () => {
         <div>
             <div className={css.section_block}>
                 <section className={css.promo}>
-                    <h1 className={css.promo_h1}>
-                        Оценивайте кандидатов быстро, легко и уверенно.
-                    </h1>
+                    <div>
+                        <h1 className={css.promo_h1}>
+                            Оценивайте кандидатов быстро, легко и уверенно.
+                        </h1>
+                        <p className={css.promo_p}>
+                            Информация о причинах блокировки была выслана на Ваш контактный E-mail.Также Вы можете связаться с нашей службой технической поддержки с помощью раздела "Помощь и поддержка" в панели управления хостингом или любым удобным для Вас способом.
+                        </p>
+                    </div>
+                    <div className={css.promo_pic}>
+
+                    </div>
+
                 </section>
             </div>
 
-            <section className={css.promoWithMan_section_block}>
-
+            <section className={css.promo_white}>
+                <div className={css.promo_white_flex}>
+                    <h1 className={css.promo_white_h1}>
+                        Собеседуйся эффективно с нашей помощью и бла бла бла бла.
+                    </h1>
+                    <div className={css.promo_white_pic}></div>
+                </div>
             </section>
         </div>
     )
---
+++ front/src/components/Login/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import css from "./index.module.css"
+
+import { useState } from 'react';
+
+
+
+const Login: React.FC = () => {
+
+    return (
+        <div>
+            Login
+        </div>
+    )
+}
+
+
+export default Login;
