638e0a1a02c2ee7cc9f5f07f079f51e5535ec16f
Sky_Taill <vorobeichik2015@yandex.ru>
2023-08-15 02:39:26 +0300

    Add login in login-component. Start - Registry

--- front/src/components/Login/index.tsx
+++ front/src/components/Login/index.tsx
@@ -6,19 +6,78 @@ import { useState } from 'react';
 
 
 const Login: React.FC = () => {
-    const [autorize, setAutorize] = useState(false)
-    const [loginBoolen, setLoginBoolen] = useState(false)
-    const [passwordBoolen, setPasswordBoolen] = useState(false)
-    const [login, setLogin] = useState("")
-    const [password, setPassword] = useState("")
+    // const [autorize, setAutorize] = useState(false)
+    // const [loginBoolen, setLoginBoolen] = useState(false)
+    // const [passwordBoolen, setPasswordBoolen] = useState(false)
+
     const [blockMenuLogin, setBlockMenuLogin] = useState(true)
     // const navigate = useNavigate();
     // const dispatch = useAppDispatch();
 
 
+    const onClickLogin = () => {
+        setBlockMenuLogin(true)
+    }
+
+    const onClickRegistry = () => {
+        setBlockMenuLogin(false)
+    }
+
+
+    return (
+        <section className={css.login_flex}>
+            <h1 className={css.login_h1}>Для начала работы с сервисом, необходимо зарегестрироваться или войти.</h1>
+            <div className={css.loginBlock}>
+                <div className={css.loginMenu}>
+                    <div className={css.flex_block}>
+                        <div>
+                            <button className={blockMenuLogin ? css.logInText : css.NoLogInText} onClick={onClickLogin}>
+                                Войти
+                            </button>
+                            <div className={blockMenuLogin ? css.loginLine : css.NoLoginLine}>
+                            </div>
+
+                        </div>
+                        <div className={css.loginMenu}>
+                            <button className={blockMenuLogin ? css.NoRegistrMenuText : css.registrMenuText} onClick={onClickRegistry}>
+                                Зарегистрироваться
+                            </button>
+                            <div className={blockMenuLogin ? css.NoRegistrLine : css.registrLine}>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {blockMenuLogin ? <LoginBlock /> : <Registry />}
+
+
+                </div>
+            </div>
+        </section>
+    )
+
+
+    function Registry() {
+
+
+
+        return (
+            <div>
+
+            </div>
+        )
+
+    }
+}
+const LoginBlock: React.FC = () => {
+    const [loginBoolen, setLoginBoolen] = useState(false)
+    const [passwordBoolen, setPasswordBoolen] = useState(false)
+    const [login, setLogin] = useState("")
+    const [password, setPassword] = useState("")
+
 
     const onChangeLogin = (e: React.FormEvent<HTMLInputElement>) => {
-
         var a: boolean
         if (e.currentTarget.value.startsWith("+")) {
             var patt = new RegExp(
@@ -54,76 +113,33 @@ const Login: React.FC = () => {
 
     const onClick = () => {
         console.log("dsada")
-        if (loginBoolen !== false && passwordBoolen !== false) {
-            // postGetToken()
-        }
+        // if (loginBoolen !== false && passwordBoolen !== false) {
+        //     // postGetToken()
+        // }
     }
+
 
     return (
-        <section className={css.login_flex}>
-            <h1 className={css.login_h1}>Для начала работы с сервисом, необходимо зарегестрироваться или войти.</h1>
-            <div className={css.loginBlock}>
-                <div className={css.loginMenu}>
-                    <div className={css.flex_block}>
-                        <div>
-                            <button className={blockMenuLogin ? css.logInText : css.NoLogInText}>
-                                Войти
-                            </button>
-                            <div className={blockMenuLogin ? css.loginLine : css.NoLoginLine}>
-                            </div>
+        <div>
+            <div className={css.loginMargin}>
+                <p className={css.textLoginBox} style={{ marginTop: "40px" }}>Логин или номер телефона:</p>
 
-                        </div>
-                        <div className={css.loginMenu}>
-                            <button className={blockMenuLogin ? css.NoRegistrMenuText : css.registrMenuText}>
-                                Зарегистрироваться
-                            </button>
-                            <div className={blockMenuLogin ? css.NoRegistrLine : css.registrLine}>
+                <input type="text" className={loginBoolen ? css.input : css.inputErr} onChange={onChangeLogin} />
 
-                            </div>
-                        </div>
+                {loginBoolen ? null : (<p className={css.textErr}>Введите корректные данные</p>)}
 
-                    </div>
-                    <p className={css.textLoginBox} style={{ marginTop: "40px" }}>Логин или номер телефона:</p>
+                <p className={css.textLoginBox} style={{ marginTop: "40px" }}>Пароль:</p>
+                <input type="password" className={passwordBoolen ? css.input : css.inputErr} onChange={onChangePassword} />
 
-                    <input type="text" className={loginBoolen ? css.input : css.inputErr} onChange={onChangeLogin} />
-
-                    {loginBoolen ? null : (<p className={css.textErr}>Введите корректные данные</p>)}
-
-                    <p className={css.textLoginBox} style={{ marginTop: "40px" }}>Пароль:</p>
-                    <input type="password" className={passwordBoolen ? css.input : css.inputErr} onChange={onChangePassword} />
-
-                    {passwordBoolen ? null : (<p className={css.textErr}>Введите корректные данные</p>)}
-
-
-                    {loginBoolen ? (passwordBoolen ? <button className={css.buttonLogInValidTrue} onClick={onClick} style={{ marginTop: "40px" }}>Войти</button>
-                        : <button className={css.buttonLogIn} style={{ marginTop: "40px" }}>Войти</button>) : <button className={css.buttonLogIn} style={{ marginTop: "40px" }} >Войти</button>
-                    }
-                </div>
+                {passwordBoolen ? null : (<p className={css.textErr}>Введите корректные данные</p>)}
             </div>
-        </section>
+            {loginBoolen ? (passwordBoolen ? <button className={css.buttonLogInValidTrue} onClick={onClick} style={{ marginTop: "40px" }}>Войти</button>
+                : <button className={css.buttonLogIn} style={{ marginTop: "40px" }}>Войти</button>) : <button className={css.buttonLogIn} style={{ marginTop: "40px" }} >Войти</button>
+            }
+        </div>
     )
-
-
-    function Login() {
-
-
-        if (login) {
-
-            return (
-                <div>
-
-                </div>
-            )
-        } else {
-            return (
-                <div>
-
-                </div>
-            )
-        }
-    }
-
 }
+
 
 
 export default Login;
